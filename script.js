87841c2bb0f929e0956166bb1144ced0d2975cc5
chew01 <chewjh19@gmail.com>
2021-12-01 10:34:12 +0800

    Completed sketch feature

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ function createGrid(squaresPerSide = 256) {
   for (let i = 0; i < squaresPerSide ** 2; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.setAttribute('data-timeshovered', '0');
     container.appendChild(square);
   }
 }
 
+function randomRGB() {
+  return Math.floor(Math.random() * 256);
+}
+
 function hoverIn() {
-  this.classList.add("hovered");
+  this.dataset.timeshovered++;
+  this.style.backgroundColor = `rgba(0,0,0,${Number(this.dataset.timeshovered) / 10})`;
 }
 
 function hoverOut() {
-  this.classList.remove("hovered");
+  //this.style.backgroundColor = `rgb(255, 255, 255)`;
+  //this.classList.remove("hovered");
 }
 
 function setHoverProperty() {
@@ -26,6 +33,10 @@ function newGrid() {
   let squaresPerSide = Number(
     prompt("Number of squares per side of new grid?")
   );
+  if (squaresPerSide > 100) {
+    alert('Maximum is 100!');
+    return;
+  }
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
